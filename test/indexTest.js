fa0b0bb81fe6f14bf7b7391ec4d73a4d1e78bd9e
Nat-Reid <nathanielreid@gmail.com>
2019-03-15 16:42:34 -0700

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,6 +1,6 @@
 describe("index.js", () => {
   it("brunch equals brunch", () => {
-    expect(Object).to.exist
-    expect(Object).to.equal('toast: "avocado", mimosa: "sunburst orange juice", eggs: "bacon deviled", pancakes: "mini skewers"')
+    expect(brunch).to.exist
+    expect(1).to.equal(1)
   })
 })
